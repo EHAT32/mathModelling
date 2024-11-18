ae52711428e9f1db45d4f0683034d111011f4422
Roman Kozin <enatik14@gmail.com>
2024-11-18 16:51:06 +0300

    upd

--- hw1/metrics.py
+++ hw1/metrics.py
@@ -4,9 +4,9 @@ from itertools import combinations
 
 def f_1(data : pd.DataFrame):
     centroids = data.groupby('label').mean()
-    centroids = centroids.rename(columns={'0' : 'x', '1' : 'y'})
+    centroids = centroids.rename(columns={0 : 'x', 1 : 'y'})
     new_df = data.copy(deep=True)
-    new_df = new_df.rename(columns={'0' : 'x', '1' : 'y'})
+    new_df = new_df.rename(columns={0 : 'x', 1 : 'y'})
     new_df = new_df.merge(centroids, on='label', suffixes=('', '_centroid'))
     new_df['squared_dist'] = (new_df['x'] - new_df['x_centroid'])**2 + (new_df['y'] - new_df['y_centroid'])**2
     total_sum = new_df.groupby('label')['squared_dist'].sum().sum()
@@ -22,14 +22,14 @@ def calculate_squared_distances(group):
 def f_2(data : pd.DataFrame):
 
     new_df = data.copy(deep=True)
-    new_df = new_df.rename(columns={'0' : 'x', '1' : 'y'})
+    new_df = new_df.rename(columns={0 : 'x', 1 : 'y'})
     group = new_df.groupby('label')
     total_dist = group.apply(calculate_squared_distances)
     return total_dist.sum()
 
 def f_3(data):
     new_df = data.copy(deep=True)
-    new_df = new_df.rename(columns={'0' : 'x', '1' : 'y'})
+    new_df = new_df.rename(columns={0 : 'x', 1 : 'y'})
     group = new_df.groupby('label')
     centroids = group.mean()
     label_count = new_df['label'].value_counts().sort_index()
@@ -45,10 +45,9 @@ def f_3(data):
 def _dist(p1, p2):
     return (p1.x - p2.x)**2 + (p1.y - p2.y)**2
 
-path = "./hw1/complete.csv"
+path = "./hw1/single.csv"
 
 df = pd.read_csv(path)
-
-res = f_2(df)
+print(df)
 
 print(f_1(df), f_2(df), f_3(df))

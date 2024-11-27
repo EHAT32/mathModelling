6ba37037fbf47b6628dbe3a9eb479f382a97467e
Roman Kozin <enatik14@gmail.com>
2024-11-27 13:05:38 +0300

    added fire spread

--- hw2/main.py
+++ hw2/main.py
@@ -1,8 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import os
+import time
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 class ForestField:
-    def __init__(self, n, p = 0.3) -> None:
+    def __init__(self, n=5, p = 0.3) -> None:
         self.n = n
         self.field = np.zeros((self.n, self.n), dtype=np.int32)
         self.p = p
@@ -10,12 +14,44 @@ class ForestField:
     def step(self):
         rand_vals = np.random.rand(*self.field.shape)
         
-        self.field[rand_vals > self.p and self.field == 1] = -1
+        #lightning strike
+        self.field[np.logical_and(rand_vals > self.p, self.field == 1)] = -1
+        clear_console()
+        print(self.field)
+        time.sleep(1)
+        #fire spread
         self.fireSpread()
-        self.field[rand_vals < self.p and self.field == 0]
+        clear_console()
+        print(self.field)
+        time.sleep(1)
+        #fire extinguish
+        self.field[self.field == -1] = 0
+        clear_console()
+        print(self.field)
+        time.sleep(1)
+        #forest growth
+        self.field[rand_vals < self.p] = 1
+        clear_console()
+        print(self.field)
+        time.sleep(1)
         
     def fireSpread(self):
-        pass
-        
-        
-    
+        dx = [-1, 0, 1, 0]
+        dy = [0, 1, 0, -1]
+        for i in range(self.n):
+            for j in range(self.n):
+                neighbours = []
+                for k in range(len(dx)):
+                    if not ( 0 <= i + dx[k] < self.n and 0 <= j + dy[k] < self.n):
+                        continue
+                    if self.field[i + dx[k]][j + dy[k]] == 0 or self.field[i][j] == 0:
+                        continue
+                    neighbours.append(self.field[i + dx[k]][j + dy[k]])
+                if len(neighbours) > 0:
+                    self.field[i][j] = min(neighbours)
+                    
+n = 5
+forest = ForestField(n)
+
+for i in range(10):
+    forest.step()

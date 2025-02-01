0f3890288b5695290ec9b3527e7b1a5945bc7302
Roman Kozin <enatik14@gmail.com>
2025-02-01 13:29:12 +0300

    plot upd

--- hw6/main.py
+++ hw6/main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 class BananaRepublic:
-    def __init__(self, s = 0.2, d = 0.1, workforce = 100, M_0 = 100):
+    def __init__(self, s = 0.2, d = 0.1, workforce = 10000, M_0 = 100):
         self.s = s
         self.d = d
         self.workforce = workforce
@@ -14,7 +14,7 @@ class BananaRepublic:
         self.time = [0]
          
     def update(self):
-        self.O.append(int(self.workforce * np.sqrt(self.M[-1])))
+        self.O.append(int(np.sqrt(self.workforce * self.M[-1])))
         self.I.append(self.s * self.O[-1])
         self.C.append(self.O[-1] - self.I[-1])
         self.M.append(int(self.M[-1] * (1 - self.d) + self.I[-1]))
@@ -23,14 +23,18 @@ class BananaRepublic:
         else:
             self.time.append(self.time[-1] + 1)
             
+    def calcBalance(self):
+        return self.workforce * self.s / self.d        
+            
 rep = BananaRepublic()
 
 for _ in range(200):
     rep.update()
     
-plt.plot(rep.time, rep.M, label="Кол-во машин")
+pred_line = [rep.calcBalance()] * len(rep.time)
+    
 plt.plot(rep.time[:-1], rep.O, label="Объём производства")
-plt.plot(rep.time[:-1], rep.I, label="Инвестиции")
+plt.plot(rep.time, pred_line, '--r', label=f'Баланс по Солоу: {int(rep.calcBalance())}')
 plt.legend()
 plt.show()
             
